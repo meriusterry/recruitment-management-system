d91fd7f35542e4be7930c998800b431162c2bbac
Terry27 <155521738+ASEMAHLE27@users.noreply.github.com>
2025-06-10 12:42:38 +0200

    fixed relationship

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -26,34 +26,32 @@ class VacancyController extends Controller
 
     public function index()
     {
-        $data = Vacancy::orderBy('created_at', 'desc')->get();  // Sort by created_at in descending order
+        $data = Vacancy::orderBy('created_at', 'desc')->get();
         return view('application', compact('data'));
     }
 
     public function career()
     {
         $data = Vacancy::where('closing_date', '>=', Carbon::today())
-            ->orderBy('created_at', 'desc')  // Sort by created_at in descending order
+            ->orderBy('created_at', 'desc')
             ->get();
         return view('career', compact('data'));
     }
     public function welcome()
     {
         $data = Vacancy::where('closing_date', '>=', Carbon::today())
-            ->orderBy('created_at', 'desc')  // Sort by created_at in descending order
+            ->orderBy('created_at', 'desc')
             ->get();
         return view('welcome', compact('data'));
     }
 
     public function destroy($id)
     {
-        // Find the vacancy by its ID
         $vacancy = Vacancy::findOrFail($id);
 
-        // Delete the vacancy
+
         $vacancy->delete();
 
-        // Redirect back with a success message
         return redirect()->route('application')->with('success', 'Vacancy deleted successfully.');
     }
 }
